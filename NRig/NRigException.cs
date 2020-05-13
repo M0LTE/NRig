efe1b7041f6736555561ec02687e7af74bd69ce6
Tom <tj@tjej.uk>
2020-05-13 19:28:31 +0100

    Add NRigException

---
+++ NRig/NRigException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NRig
+{
+    public class NRigException : Exception
+    {
+        public NRigException()
+        {
+        }
+
+        public NRigException(string message) : base(message)
+        {
+        }
+
+        public NRigException(Exception innerException) : base(innerException.Message, innerException)
+        {
+        }
+
+        public NRigException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
