658d312f2783e295a15ba664a0b6bfeb78f19923
Tom <tj@tjej.uk>
2020-05-17 19:54:12 +0100

    Implement Yaesu frequency output

---
+++ NRig.Tests/FrequencyTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace NRig.Tests
+{
+    public class FrequencyTests
+    {
+        [Theory]
+        [InlineData(-100,      "-100")]
+        [InlineData(999999999, "999.999.99")]
+        [InlineData(130472000, "130.472.00")]
+        [InlineData(130472300, "130.472.30")]
+        [InlineData(130472306, "130.472.30")]
+        [InlineData(30472306,  " 30.472.30")]
+        [InlineData(3047230,   "  3.047.23")]
+        [InlineData(909000,    "    909.00")]
+        [InlineData(100000,    "    100.00")]
+        [InlineData(99000,     "     99.00")]
+        [InlineData(9900,      "      9.90")]
+        [InlineData(900,       "        90")]
+        [InlineData(90,        "         9")]
+        [InlineData(9,         "         0")]
+        public void TestYaesuStandard(double hz, string expected)
+        {
+            var freq = Frequency.Hz(hz);
+            Assert.Equal(expected, freq.ToString(FrequencyStyle.Yaesu, 8));
+        }
+    }
+}
--- NRig/Frequency.cs
+++ NRig/Frequency.cs
@@ -34,11 +34,74 @@ namespace NRig
         public static Frequency GHz(decimal gHz) => new Frequency { ValueHz = gHz * 1000000000 };
         public static Frequency THz(decimal tHz) => new Frequency { ValueHz = tHz * 1000000000000 };
 
+        public string ToString(FrequencyStyle frequencyStyle, int digits)
+        {
+            if (frequencyStyle == FrequencyStyle.Yaesu)
+            {
+                var mHz = (int)(ValueHz / 1000000);
+                var kHz = (int)((ValueHz - 1000000 * mHz) / 1000);
+                var hz = ValueHz - 1000000 * mHz - 1000 * kHz;
+                var dHz = (int)(hz / 10);
+
+                if (ValueHz < 0)
+                {
+                    return ValueHz.ToString();
+                }
+
+                string formatted;
+                if (ValueHz >= 100000000) // > 100MHz
+                {
+                    formatted = $"{mHz:#}.{kHz:000}.{dHz:00}";
+                }
+                else if (ValueHz >= 10000000) // > 10MHz
+                {
+                    formatted = $" {mHz:#}.{kHz:000}.{dHz:00}";
+                }
+                else if (ValueHz >= 1000000) // > 1MHz
+                {
+                    formatted = $"  {mHz:#}.{kHz:000}.{dHz:00}";
+                }
+                else if (ValueHz >= 100000) // > 100KHz
+                {
+                    formatted = $"    {kHz:000}.{dHz:00}";
+                }
+                else if (ValueHz >= 10000) // > 10 KHz
+                {
+                    formatted = $"     {kHz:00}.{dHz:00}";
+                }
+                else if (ValueHz >= 1000) // > 1KHz
+                {
+                    formatted = $"      {kHz:0}.{dHz:00}";
+                }
+                else if (ValueHz >= 100)
+                {
+                    formatted = $"        {dHz:00}";
+                }
+                else if (ValueHz >= 10)
+                {
+                    formatted = $"         {dHz:0}";
+                }
+                else
+                {
+                    formatted = $"         {dHz:0}";
+                }
+
+                return formatted;
+            }
+
+            throw new NotImplementedException(frequencyStyle.ToString());
+        }
+
         private class FrequencyConverter : JsonConverter<Frequency>
         {
             public override Frequency Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => Frequency.Hz(reader.GetDecimal());
 
             public override void Write(Utf8JsonWriter writer, Frequency value, JsonSerializerOptions options) => writer.WriteNumberValue(value.ValueHz);
         }
+    }
+
+    public enum FrequencyStyle
+    {
+        Yaesu
     }
 }
